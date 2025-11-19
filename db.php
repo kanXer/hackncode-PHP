1d5e8152b900fbd0b1f7d24641889c350768666d
Sahil Srivastav <user.kanxer@gmail.com>
2025-11-19 16:24:29 +0530

    Change database connection parameters to local

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$host = "sql212.infinityfree.com";
-$user = "if0_40164900";
-$pass = "itsLala210600";
-$dbname = "if0_40164900_epiz_123456_kanxer";
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "kanxer-news";
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 
